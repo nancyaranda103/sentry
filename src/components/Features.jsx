6ae0566c78bd42bb8162b5bf888b4ebf87ee3666
Mr darrk <nancyaranda103@gmail.com>
2025-09-14 13:33:52 +0100

    cool

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -14,7 +14,7 @@ const {left, top, width, height} = itemRef.current.getBoundingClientRect();
 
 const relativeX = (e.clientX - left) / width;
 const relativeY =  (e.clientY - top) / height;
-const tiltX = (relativeY - 0.5 )* 10;
+const tiltX = (relativeY - 0.5 )* 10;gir 
 const tiltY = (relativeX - 0.5 ) *- 10;
 
 const newTransform = `perspective(700px) rotateX(${tiltX}deg) rotateY(${tiltY}deg) scale3d(0.95, 0.95, 0.95)` 
---
+++ src/components/cool.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const cool = () => {
+  return (
+    <div>cool</div>
+  )
+}
+
+export default cool
